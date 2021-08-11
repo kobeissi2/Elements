ba57a1c5094dbcca3447197cb7d368950d8fc80e
Moe <mohamadk@umich.edu>
2021-08-11 10:31:41 -0400

    Fixed gradle upgrade

--- app/src/main/java/com/kobeissidev/elements/model/Tag.kt
+++ app/src/main/java/com/kobeissidev/elements/model/Tag.kt
@@ -1,22 +1,31 @@
 package com.kobeissidev.elements.model
 
+import android.os.Parcelable
 import com.squareup.moshi.Json
+import com.squareup.moshi.JsonClass
+import kotlinx.parcelize.Parcelize
 
 /**
  * Tag object from LastFM
  */
-data class Tag(@field:Json(name = "toptags") val topTags: TopTags)
+@Parcelize
+@JsonClass(generateAdapter = true)
+data class Tag(@field:Json(name = "toptags") val topTags: TopTags) : Parcelable
 
 /**
  * Tag object inside the Tag object
  * Only the used properties are being de-serialized.
  * @param tag contains all of the tags.
  */
-data class TopTags(@field:Json(name = "tag") val tag: List<TagItem>)
+@Parcelize
+@JsonClass(generateAdapter = true)
+data class TopTags(@field:Json(name = "tag") val tag: List<TagItem>) : Parcelable
 
 /**
  * Tag object that contains the information we need
  * Only the used properties are being de-serialized.
  * @param name contains name of the tag.
  */
-data class TagItem(@field:Json(name = "name") val name: String)
+@Parcelize
+@JsonClass(generateAdapter = true)
+data class TagItem(@field:Json(name = "name") val name: String) : Parcelable
